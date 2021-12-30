279aaa93bd061642ce2ee2318039a82bcac5d1f2
quoc thai <hoquocthai204@gmail.com>
2021-12-30 09:24:28 +0700

    update

--- Study_MySQL/note_MySQL.sql
+++ Study_MySQL/note_MySQL.sql
@@ -7,4 +7,4 @@ delete from mydatabase.user_notification
 where user_id = 1;
 
 update mydatabase.user_notification
-set content = 'good bye' where user_id = 0
+set content = 'good bye' where user_id = 0;
