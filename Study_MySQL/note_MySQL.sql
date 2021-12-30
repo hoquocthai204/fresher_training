d236530b6b2cbf8ff62175440b85db0e577cf6c6
quoc thai <hoquocthai204@gmail.com>
2021-12-30 13:39:15 +0700

    MySQL_update_statement

--- Study_MySQL/note_MySQL.sql
+++ Study_MySQL/note_MySQL.sql
@@ -7,4 +7,9 @@ delete from mydatabase.user_notification
 where user_id = 1;
 
 update mydatabase.user_notification
-set content = 'good bye' where user_id = 0;
+set content = 'good bye', create_date='2021-12-30' where user_id <1;
+
+select content, title, user_id from mydatabase.user_notification 
+inner join mydatabase.user
+where user_notification.user_id = user.id and user_id<3
+order by user_id desc;
